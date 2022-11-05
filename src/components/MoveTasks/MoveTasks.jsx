23da693643da5e919c649b0fa8b333b6fc0be12c
nikolozb <berose995@gmail.com>
2022-11-05 10:09:03 +0400

    fixed: move tasks

--- src/components/MoveTasks/MoveTasks.jsx
+++ src/components/MoveTasks/MoveTasks.jsx
@@ -2,10 +2,12 @@ import React from "react";
 import "./MoveTasks.styles.scss";
 
 const MoveTasks = ({ id, todos, setTodos }) => {
-  const onMoveUpHandler = () => {
+  const onMoveHandler = (isUp) => {
     const currentElement = todos.find((item) => item.id === id);
-    const indexOfCurrentElement = todos.indexOf(currentElement);
-    const indexOfMovedElement = indexOfCurrentElement - 1;
+    let indexOfCurrentElement = todos.indexOf(currentElement);
+    let indexOfMovedElement = isUp
+      ? indexOfCurrentElement - 1
+      : indexOfCurrentElement + 1;
     if (indexOfMovedElement === -1) {
       return;
     } else {
@@ -16,22 +18,14 @@ const MoveTasks = ({ id, todos, setTodos }) => {
       setTodos(newArray);
     }
   };
-  // move down
-  const onMoveDownHandler = () => {
-    const currentElement = todos.find((item) => item.id === id);
-    const indexOfCurrentElement = todos.indexOf(currentElement);
-    const indexOfMovedElement = indexOfCurrentElement + 1;
-    const newArray = todos.filter((item) => {
-      return todos.indexOf(item) !== indexOfCurrentElement;
-    });
-    newArray.splice(indexOfMovedElement, 0, currentElement);
-    setTodos(newArray);
-  };
 
   return (
     <div className="up-down-buttons">
-      <span onClick={onMoveUpHandler} className="arrow-btn up"></span>
-      <span onClick={onMoveDownHandler} className="arrow-btn down"></span>
+      <span onClick={() => onMoveHandler(true)} className="arrow-btn up"></span>
+      <span
+        onClick={() => onMoveHandler(false)}
+        className="arrow-btn down"
+      ></span>
     </div>
   );
 };
